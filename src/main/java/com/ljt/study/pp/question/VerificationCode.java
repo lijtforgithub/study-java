a748e9be444c4c05ef56514d8a36913b5db00162
李敬堂 <lijingtang@sanweiguoye.onaliyun.com>
2021-05-31 11:04:28 +0800

    Update VerificationCode.java

--- src/main/java/com/ljt/study/pp/question/VerificationCode.java
+++ src/main/java/com/ljt/study/pp/question/VerificationCode.java
@@ -1,5 +1,9 @@
 package com.ljt.study.pp.question;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.stream.IntStream;
+
 /**
  * @author LiJingTang
  * @date 2021-05-25 09:00
@@ -7,8 +11,20 @@ package com.ljt.study.pp.question;
 public class VerificationCode {
 
     public static void main(String[] args) {
-        String code = String.valueOf((int) ((Math.random() * 9 + 1) * Math.pow(10, 5)));
-        System.out.println(code);
+        int count = 100_0000;
+
+        long start = System.currentTimeMillis();
+        IntStream.rangeClosed(0, count).forEach(i -> {
+            String code = String.valueOf((int) ((Math.random() * 9 + 1) * Math.pow(10, 5)));
+        });
+        System.out.println(System.currentTimeMillis() - start);
+
+        start = System.currentTimeMillis();
+        IntStream.rangeClosed(0, count).forEach(i -> {
+            String code = RandomStringUtils.randomAlphanumeric(6);
+        });
+        System.out.println(System.currentTimeMillis() - start);
+
     }
 
 }
